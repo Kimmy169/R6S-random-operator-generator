02d32685de18ad0bb74f72a66ba555dfc7608b79
Kimmy169 <83825284+Kimmy169@users.noreply.github.com>
2021-08-02 11:07:06 +0200

    Add project files.

---
+++ Rainbow 6 Siege random operator generator/App.cs
@@ -0,0 +1,50 @@
+﻿namespace Rainbow_6_Siege_random_operator_generator
+{
+    using System;
+
+    /// <summary>
+    /// Start app.
+    /// </summary>
+    public class App
+    {
+        /// <summary>
+        /// Main.
+        /// </summary>
+        /// <param name="args">Console arguments.</param>
+        public static void Main(string[] args)
+        {
+            string[] attackers = new string[30] { "Sledge", "Thatcher", "Ash", "Thermite", "Montagne", "Twitch", "Blitz", "IQ", "Fuze", "Glaz", "Buck", "Blackbeard", "Capitao", "Hibana", "Jackal", "Ying", "Zofia", "Dokkaebi", "Finka", "Lion", "Maverick", "Nomad", "Gridlock", "Nokk", "Amaru", "Kali", "Iana", "Ace", "Zero", "Flores" };
+
+            string[] defenders = new string[30] { "Smoke", "Mute", "Castle", "Pulse", "Rook", "Doc", "Bandit", "Jäger", "Kapkan", "Tachanka", "Frost", "Valkyrie", "Caveira", "Echo", "Mira", "Lesion", "Ela", "Vigil", "Maestro", "Alibi", "Clash", "Kaid", "Mozzie", "Warden", "Goyo", "Wamai", "Oryx", "Melusi", "Aruni", "Thunderbird" };
+
+            Console.Write("Imput your nickname: ");
+            string nickname = Console.ReadLine();
+
+            Console.WriteLine("\nHello " + nickname);
+
+            Console.Write("\nChoose side (A: for attakcer | D: for defender): ");
+            string side = Console.ReadLine();
+
+            if (side is "a" || side is "A")
+            {
+                Random random = new Random();
+                int pick = random.Next(0, attackers.Length);
+                string randomOperator = attackers[pick];
+
+                Console.WriteLine("\nYour operator is " + randomOperator);
+            }
+            else if (side is "d" || side is "D")
+            {
+                Random random = new Random();
+                int pick = random.Next(0, defenders.Length);
+                string randomOperator = defenders[pick];
+
+                Console.WriteLine("\nYour operator is " + randomOperator);
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid character");
+            }
+        }
+    }
+}
